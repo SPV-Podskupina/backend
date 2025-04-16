1d2a42c6f9035d867d35faab45f27d832f1256f0
Matija Pajenk <matija.pajenk@student.um.si>
2025-04-16 18:10:10 +0200

    SPV-58 add cosmetic create

--- controllers/cosmeticController.js
+++ controllers/cosmeticController.js
@@ -29,7 +29,7 @@ module.exports = {
     show: function (req, res) {
         var id = req.params.id;
 
-        CosmeticModel.findOne({_id: id}, function (err, cosmetic) {
+        CosmeticModel.findOne({ _id: id }, function (err, cosmetic) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when getting cosmetic.',
@@ -50,23 +50,34 @@ module.exports = {
     /**
      * cosmeticController.create()
      */
-    create: function (req, res) {
-        var cosmetic = new CosmeticModel({
-			type : req.body.type,
-			resource_path : req.body.resource_path,
-			value : req.body.value
-        });
+    create: async function (req, res) {
+        const { name, type, resource_path, value } = req.body;
 
-        cosmetic.save(function (err, cosmetic) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when creating cosmetic',
-                    error: err
+        try {
+            const existingCosmetic = await CosmeticModel.findOne({ name });
+
+            if (existingCosmetic) {
+                return res.status(409).json({
+                    message: `Cosmetic with name "${name}" already exists.`
                 });
             }
 
+            const cosmetic = new CosmeticModel({
+                name,
+                type,
+                resource_path,
+                value
+            });
+
+            await cosmetic.save();
             return res.status(201).json(cosmetic);
-        });
+
+        } catch (error) {
+            console.error('Error creating cosmetic:', error);
+            return res.status(500).json({
+                message: "Error creating cosmetic"
+            });
+        }
     },
 
     /**
@@ -75,7 +86,7 @@ module.exports = {
     update: function (req, res) {
         var id = req.params.id;
 
-        CosmeticModel.findOne({_id: id}, function (err, cosmetic) {
+        CosmeticModel.findOne({ _id: id }, function (err, cosmetic) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when getting cosmetic',
@@ -90,9 +101,9 @@ module.exports = {
             }
 
             cosmetic.type = req.body.type ? req.body.type : cosmetic.type;
-			cosmetic.resource_path = req.body.resource_path ? req.body.resource_path : cosmetic.resource_path;
-			cosmetic.value = req.body.value ? req.body.value : cosmetic.value;
-			
+            cosmetic.resource_path = req.body.resource_path ? req.body.resource_path : cosmetic.resource_path;
+            cosmetic.value = req.body.value ? req.body.value : cosmetic.value;
+
             cosmetic.save(function (err, cosmetic) {
                 if (err) {
                     return res.status(500).json({
